7f1363766ddaea8cfb858da971e186dff28fe657
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-08-30 19:47:53 -0600

    feat: Add Startify plugin

---
+++ lua/plugins/startify.lua
@@ -0,0 +1,27 @@
+local config = function()
+  vim.keymap.set('n', '<leader>vs', [[<CMD> Startify <CR>]], { desc = "Startify" })
+  vim.keymap.set('n', '<leader>bq', [[<CMD> %bd <BAR> Startify <CR>]], { desc = "Startify" })
+end
+
+return({
+    "mhinz/vim-startify",
+    config = config,
+    init = function()
+        vim.cmd([[
+          let g:ascii = [
+                \ " ██▒   █▓ ██▓ ███▄ ▄███▓ ",
+                \ "▓██░   █▒▓██▒▓██▒▀█▀ ██▒ ",
+                \ " ▓██  █▒░▒██▒▓██    ▓██░ ",
+                \ "  ▒██ █░░░██░▒██    ▒██  ",
+                \ "   ▒▀█░  ░██░▒██▒   ░██▒ ",
+                \ "   ░ ▐░  ░▓  ░ ▒░   ░  ░ ",
+                \ "   ░ ░░   ▒ ░░  ░      ░ ",
+                \ "     ░░   ▒ ░░      ░    ",
+                \ "      ░   ░         ░    ",
+                \ "     ░                   ",
+                \ ]
+          let g:startify_custom_header ='startify#pad(g:ascii + startify#fortune#boxed())'
+          let g:startify_lists = [{ 'type': 'files',     'header': ['   Recent']  }]
+        ]])
+    end
+})
