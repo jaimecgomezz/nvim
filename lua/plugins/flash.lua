7dc36267b4cea41e4d5503d6564250287122a6f8
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 14:24:04 -0600

    style: Format flash.nvim file

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,29 +1,57 @@
 local jump_to_line = function()
-    require("flash").jump({
-        search = { mode = "search", max_length = 0 },
-        label = { after = { 0, 0 } },
-        pattern = "^"
-    })
+	require("flash").jump({
+		search = { mode = "search", max_length = 0 },
+		label = { after = { 0, 0 } },
+		pattern = "^",
+	})
 end
 
-return({
-  "folke/flash.nvim",
-  event = "VeryLazy",
-    opts = {
-        modes = {
-            search = {
-                enabled = true
-            },
-            char = {
-                jump_labels = true
-            }
-        }
-    },
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<C-j>", mode = { "n", "x", "o" }, jump_to_line, desc = "Jump to line" }
-  },
-})
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {
+		modes = {
+			search = {
+				enabled = true,
+			},
+			char = {
+				jump_labels = true,
+			},
+		},
+	},
+	keys = {
+		{
+			"s",
+			mode = { "n", "x" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+		{
+			"R",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+		{ "<C-j>", mode = { "n", "x", "o" }, jump_to_line, desc = "Jump to line" },
+	},
+}
