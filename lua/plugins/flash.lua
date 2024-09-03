a776e19f3d25a72e0837664c25c11e922359f5e8
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 14:54:56 -0600

    fix: Remove unnecessary flash mappings

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -29,28 +29,12 @@ return {
 			desc = "Flash",
 		},
 		{
-			"S",
-			mode = { "n", "x" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
 			"r",
 			mode = "o",
 			function()
 				require("flash").remote()
 			end,
 			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
 		},
 		{ "<C-j>", mode = { "n", "x", "o" }, jump_to_line, desc = "Jump to line" },
 	},
