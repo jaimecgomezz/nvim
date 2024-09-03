1ef4aceb1302ab3dd98d525cf1a3d264653d4ba0
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 16:06:33 -0600

    style: Format lint file

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,34 +1,33 @@
 local config = function()
-    local lint = require("lint")
+	local lint = require("lint")
 
-    lint.linters_by_ft = {
-        lua = { "luacheck" },
-        json = { "jsonlint" },
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        ruby = { "rubocop" },
-        markdown = { "markdownlint" },
-        bash = { "shellcheck" },
-        rust = { "ruff" },
-    }
+	lint.linters_by_ft = {
+		lua = { "luacheck" },
+		json = { "jsonlint" },
+		javascript = { "eslint_d" },
+		typescript = { "eslint_d" },
+		ruby = { "rubocop" },
+		markdown = { "markdownlint" },
+		bash = { "shellcheck" },
+		rust = { "ruff" },
+	}
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWritePost", "InsertLeave" },
-        { group = lint_augroup, callback = function() lint.try_lint() end, }
-    )
+	vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+		group = lint_augroup,
+		callback = function()
+			lint.try_lint()
+		end,
+	})
 
-    vim.keymap.set(
-        "n",
-        "<leader>bl",
-        function() lint.try_lint() end,
-        { desc = "Trigger linting for current file" }
-    )
+	vim.keymap.set("n", "<leader>bl", function()
+		lint.try_lint()
+	end, { desc = "Trigger linting for current file" })
 end
 
-return ({
-    "mfussenegger/nvim-lint",
-    config = config,
-    event = { "BufReadPre", "BufNewFile", },
-})
+return {
+	"mfussenegger/nvim-lint",
+	config = config,
+	event = { "BufReadPre", "BufNewFile" },
+}
