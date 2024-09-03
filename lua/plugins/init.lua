5181882463f575632357e06be9e8833f584f307b
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 14:01:03 -0600

    feat: Configure nvim-colorizer.lua plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,4 +19,10 @@ return {
 		event = "InsertEnter",
 		config = true,
 	},
+	{
+		"NvChad/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ user_default_options = { mode = "virtualtext" } })
+		end,
+	},
 }
