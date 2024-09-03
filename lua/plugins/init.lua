87d884e139f679fa7c12f771e171c68322db76f9
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 14:10:26 -0600

    feat: Add vim-wordmotion plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,6 +23,9 @@ return {
 		"andymass/vim-matchup",
 	},
 	{
+		"chaoren/vim-wordmotion",
+	},
+	{
 		"NvChad/nvim-colorizer.lua",
 		config = function()
 			require("colorizer").setup({ user_default_options = { mode = "virtualtext" } })
