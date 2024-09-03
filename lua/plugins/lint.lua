4c5b9d8da1dd389f055bf49ae8b10f2e9d70442c
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 16:19:44 -0600

    fix: Ignore lint errors

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -14,10 +14,10 @@ local config = function()
 
 	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-	vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 		group = lint_augroup,
 		callback = function()
-			lint.try_lint()
+			lint.try_lint(nil, { ignore_errors = true })
 		end,
 	})
 
