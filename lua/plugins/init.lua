149345147b1a75914c7ec912c8dbd3dbc33d7d60
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-05 13:44:59 -0600

    feat: Add reply.vim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -142,4 +142,17 @@ return {
 			vim.g.suda_smart_edit = 1
 		end,
 	},
+	{
+		"rhysd/reply.vim",
+		cmd = { "Repl", "ReplAuto" },
+		keys = {
+			{ "<leader>ra", "<CMD> ReplAuto <CR>", desc = "Open RPL", mode = { "n" } },
+			{ "<leader>rr", "<CMD> Repl <CR>", desc = "Open RPL", mode = { "n" } },
+			{ "<leader>rr", "<CMD>'<,'> Repl <CR>", desc = "Open RPL", mode = { "v" } },
+			{ "<leader>rs", "<CMD> ReplSend <CR>", desc = "Open RPL", mode = { "n" } },
+			{ "<leader>rs", "<CMD>'<,'> ReplSend <CR>", desc = "Open RPL", mode = { "v" } },
+			{ "<leader>rl", "<CMD> ReplList <CR>", desc = "List RPLs", mode = { "n" } },
+			{ "<leader>rk", "<CMD> ReplStop <CR>", desc = "Stop RPL", mode = { "n" } },
+		},
+	},
 }
