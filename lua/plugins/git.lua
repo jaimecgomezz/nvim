e2cb0325294fa3b835493c7eec5ad160cf29c130
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-07 17:04:53 -0600

    refactor: Replace mini.diff with gitsigns.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,61 @@
+local function config_signs()
+	require("gitsigns").setup({
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+
+			local function map(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			-- Navigation
+			map("n", "]h", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "]h", bang = true })
+				else
+					gitsigns.nav_hunk("next")
+				end
+			end)
+
+			map("n", "[h", function()
+				if vim.wo.diff then
+					vim.cmd.normal({ "[h", bang = true })
+				else
+					gitsigns.nav_hunk("prev")
+				end
+			end)
+
+			-- Actions
+			map("n", "<leader>hs", gitsigns.stage_hunk)
+			map("n", "<leader>hr", gitsigns.reset_hunk)
+			map("v", "<leader>hs", function()
+				gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end)
+			map("v", "<leader>hr", function()
+				gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+			end)
+			map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+			map("n", "<leader>hp", gitsigns.preview_hunk)
+			map("n", "<leader>hS", gitsigns.stage_buffer)
+			map("n", "<leader>hR", gitsigns.reset_buffer)
+			map("n", "<leader>hb", function()
+				gitsigns.blame_line({ full = true })
+			end)
+			map("n", "<leader>hd", gitsigns.diffthis)
+			map("n", "<leader>hD", function()
+				gitsigns.diffthis("~")
+			end)
+			map("n", "<leader>hx", gitsigns.toggle_deleted)
+
+			-- Text object
+			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+		end,
+	})
+
+	vim.keymap.set("n", "<leader>gd", "<CMD> Gitsigns toggle_signs <CR>", { desc = "Toggle git sigs" })
+end
+
 local config = function()
 	local neogit = require("neogit")
 
@@ -34,12 +92,6 @@ return {
 		},
 	},
 	{
-		"echasnovski/mini.diff",
-		version = "*",
-		opts = { view = { style = "sign" } },
-		keys = { { "<leader>gd", "<CMD> lua MiniDiff.toggle() <CR>", desc = "Git blame window" } },
-	},
-	{
 		"NeogitOrg/neogit",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -47,5 +99,9 @@ return {
 			"nvim-telescope/telescope.nvim",
 		},
 		config = config,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = config_signs,
 	},
 }
