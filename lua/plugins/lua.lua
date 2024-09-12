c8138920cb6f8983c231d061f46f7878c9663055
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-12 10:48:22 -0600

    feat: Add development plugins for lua

---
+++ lua/plugins/lua.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"Bilal2453/luvit-meta",
+		lazy = true,
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0,
+			})
+		end,
+	},
+}
