f2de0fb1b8ad005b0bafba2d780405ff2a1c2724
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-21 11:41:32 -0600

    refactor: Remove noice.nvim junk

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -14,47 +14,19 @@ return {
 		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
 		opts = {
 			messages = { enabled = false },
-			popupmenu = { enabled = true, backend = "cmp" },
+			popupmenu = { enabled = false },
 			notify = { enabled = true },
-			redirect = {
-				view = "popup",
-			},
 			lsp = {
-				progress = {
-					enabled = false,
-				},
-				override = {
-					-- override the default lsp markdown formatter with Noice
-					["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-					-- override the lsp markdown formatter with Noice
-					["vim.lsp.util.stylize_markdown"] = false,
-					-- override cmp documentation with Noice (needs the other options to work)
-					["cmp.entry.get_documentation"] = false,
-				},
-				hover = {
-					enabled = true,
-					silent = true, -- NOT WORKING
-				},
-				signature = {
-					enabled = true,
-					auto_open = { enabled = false },
-				},
 				message = {
 					enabled = true,
 					view = "notify",
 				},
-			},
-			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
-				command_palette = false, -- position the cmdline and popupmenu together
-				long_message_to_split = false, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = true, -- add a border to hover docs and signature help
+				progress = { enabled = false },
+				hover = { enabled = false },
+				signature = { enabled = false },
 			},
 			views = {
-				hover = { border = { style = "single" } },
 				confirm = { border = { style = "single" } },
-				popup = { border = { style = "single" } },
 			},
 		},
 	},
