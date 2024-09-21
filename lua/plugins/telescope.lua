c08f71f3d4629b8abbf250a43c18f87d4ff1b5e3
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-21 11:41:17 -0600

    feat: Improve Telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 local config = function()
 	local telescope = require("telescope")
+	local actions = require("telescope.actions")
 	local path_actions = require("telescope_insert_path")
 
 	telescope.setup({
@@ -28,20 +29,30 @@ local config = function()
 			borderchars = { "-", "|", "-", "|", "╭", "╮", "╯", "╰" },
 			mappings = {
 				n = {
-					["q"] = require("telescope.actions").close,
-					["<Esc>"] = require("telescope.actions").close,
-					["<C-q>"] = require("telescope.actions").send_selected_to_qflist,
-					["<C-Space>"] = require("telescope.actions").toggle_selection,
+					-- Exit
+					["q"] = actions.close,
+					["<Esc>"] = actions.close,
+
+					-- Selection
+					["<C-Space>"] = actions.toggle_selection,
+					["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+
+					-- Path
 					["-"] = path_actions.insert_reltobufpath_visual,
 					["="] = path_actions.insert_abspath_visual,
 				},
 				i = {
-					["<Esc>"] = require("telescope.actions").close,
+					-- Exit
+					["<Esc>"] = actions.close,
 					["<C-n>"] = { "<esc>", type = "command" },
-					["<Tab>"] = require("telescope.actions").move_selection_next,
-					["<S-Tab>"] = require("telescope.actions").move_selection_previous,
-					["<C-Space>"] = require("telescope.actions").toggle_selection,
-					["<C-q>"] = require("telescope.actions").send_selected_to_qflist,
+
+					-- Selection
+					["<C-Space>"] = actions.toggle_selection,
+					["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+
+					-- Movement
+					["<Tab>"] = actions.move_selection_next,
+					["<S-Tab>"] = actions.move_selection_previous,
 				},
 			},
 		},
@@ -67,14 +78,11 @@ return {
         { "<leader>ff", "<cmd> Telescope live_grep <CR>", desc = "Live grep" },
         { "<leader>fo", "<cmd> Telescope oldfiles <CR>", desc = "Find oldfiles" },
         { "<leader>ft", "<cmd> Telescope filetypes <CR>", desc = "Find filetypes" },
-        { "<leader>fq", "<cmd> Telescope quickfix <CR>", desc = "Find quickfix" },
         { "<leader>fr", "<cmd> Telescope registers <CR>", desc = "Find registers" },
         { "<leader>fd", "<cmd> Telescope diagnostics bufnr=0 <CR>", desc = "Find diagnostics" },
         { "<leader>fw", "<cmd> Telescope grep_string <CR>", desc = "Find word under cursor" },
         { "<leader>fc", "<cmd> Telescope colorscheme <CR>", desc = "Find colorschemes" },
         { "<leader>fh", "<cmd> Telescope help_tags <CR>", desc = "Find help tags" },
-        { "<leader>fbc", "<cmd> Telescope git_bcommits <CR>", desc = "Find buffer commits" },
-        { "<leader>fgc", "<cmd> Telescope git_commits <CR>", desc = "Find git commits" },
         { "<C-A-p>", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", desc = "Find all files" },
         { "<C-l>", "<cmd>lua require( 'telescope.builtin').find_files( { cwd = vim.fn.expand( '%:p:h') }) <CR>", desc = "Find relative files" },
         { "<C-space>", "<cmd>lua require( 'telescope.builtin').buffers( { sort_mru = true, ignore_current_buffer = true }) <CR>", desc = "Find buffers" }
