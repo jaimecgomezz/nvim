187b4bbc1a5d468f1c8f4d82b97280f5373a9a12
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-23 13:50:56 -0600

    fix: Remap cw for nvim-spider

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -22,6 +22,7 @@ return {
 		opts = { consistentOperatorPending = true },
 		-- stylua: ignore
 		keys = {
+            { "cw", "ce", remap = true },
 			{ "e", function() require("spider").motion("e") end, mode = { "n", "o", "x" } },
 			{ "w", function() require("spider").motion("w") end, mode = { "n", "o", "x" } },
 			{ "b", function() require("spider").motion("b") end, mode = { "n", "o", "x" } },
