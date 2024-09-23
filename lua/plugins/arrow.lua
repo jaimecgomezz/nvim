a6194762ed4eea7086f0240519a837cf3d971203
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-22 22:50:02 -0600

    refactor: Replace harpoon with arrow

---
+++ lua/plugins/arrow.lua
@@ -0,0 +1,39 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	lazy = false,
+	opts = {
+		hide_handbook = true,
+		mappings = {
+			edit = "e",
+			delete_mode = "d",
+			clear_all_items = "X",
+			toggle = "a",
+			open_vertical = "v",
+			open_horizontal = "s",
+			quit = "q",
+			remove = "x",
+			next_item = "]",
+			prev_item = "[",
+		},
+		window = {
+			border = "single",
+		},
+		per_buffer_config = {
+			lines = 5,
+			satellite = {
+				enable = true,
+				overlap = true,
+				priority = 1000,
+			},
+		},
+		leader_key = ";",
+		buffer_leader_key = "m",
+	},
+	-- stylua: ignore
+	keys = {
+		{ "H", function() require("arrow.persist").previous() end, desc = "Previous arrowed file" },
+		{ "L", function() require("arrow.persist").next() end, desc = "Next arrowed file" },
+        { "<C-a>", function() require("arrow.persist").toggle() end, desc = "Arrow file" },
+        { "<C-m>", function() require("arrow.commands").commands.toggle_current_line_for_buffer() end, desc = "Arrow line" },
+	},
+}
