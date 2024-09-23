9a474b72dd224e64b6424873421659a841fef416
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-22 19:45:20 -0600

    refactor: Replace kulala with rest

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,16 +1,25 @@
 return {
-	"mistweaverco/kulala.nvim",
+	"rest-nvim/rest.nvim",
 	ft = "http",
-	opts = {},
-	init = function()
+	config = function()
 		vim.filetype.add({ extension = { ["http"] = "http" } })
 	end,
     -- stylua: ignore
     keys = {
-        { "<leader>c", "", desc = "+Rest" },
-        { "<leader>cs", function() require('kulala').run() end, desc = "Send the request" },
-        { "<leader>ct", function() require('kulala').toggle_view() end, desc = "Toggle headers/body" },
-        { "<leader>cp", function() require('kulala').jump_prev() end, desc = "Jump to previous request" },
-        { "<leader>cn", function() require('kulala').jump_next() end, desc = "Jump to next request" },
+        { "<leader>s", "", desc = "+Rest" },
+        { "<leader>ss", "<CMD> Rest run <CR>", desc = "Run request under cursor" },
+        { "<leader>sS", function ()
+                vim.ui.input({ prompt = "Request name " }, function(name)
+                    if name ~= nil and name ~= "" then
+                        cmd = "Rest run " .. name
+                        vim.cmd(cmd)
+                    end
+                end)
+        end, desc = "Run names request" },
+        { "<leader>sp", "<CMD> Rest last <CR>", desc = "Run previous request" },
+        { "<leader>sl", "<CMD> Rest logs <CR>", desc = "Open rest logs" },
+        { "<leader>sc", "<CMD> Rest cookies <CR>", desc = "Open rest cookies" },
+        { "<leader>se", "<CMD> Rest env <CR>", desc = "Open rest env" },
+        { "<leader>sE", "<CMD> Rest env select <CR>", desc = "Set rest env" },
     },
 }
