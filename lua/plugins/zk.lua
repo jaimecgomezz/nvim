0b04dae923db3e3d66286ac1a098bff435ecd9b5
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-23 11:47:56 -0600

    feat: Improve zk commands

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -5,12 +5,63 @@ return {
 			picker = "telescope",
 		})
 	end,
-	-- stylua: ignore
 	keys = {
-        { "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "Open notes" },
-		{ "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "New note" },
-        { "<leader>zt", "<Cmd>ZkTags<CR>", desc = "Open notes matching tags" },
-        { "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>", desc = "Search for notes" },
+		{
+			"<leader>zz",
+			function()
+				require("zk").cd()
+			end,
+			desc = "Open notes",
+		},
+		{
+			"<leader>zi",
+			function()
+				require("zk").index()
+			end,
+			desc = "Open notes",
+		},
+		{
+			"<leader>zo",
+			function()
+				require("zk.commands").get("ZkNotes")({ sort = { "modified" } })
+			end,
+			desc = "Open notes",
+		},
+		{
+			"<leader>zn",
+			function()
+				require("zk.commands").get("ZkNew")({ dir = "idea", title = vim.fn.input("Title: ") })
+			end,
+			desc = "New note",
+		},
+		{
+			"<leader>zt",
+			function()
+				require("zk.commands").get("ZkTags")()
+			end,
+			desc = "Open notes matching tags",
+		},
+		{
+			"<leader>zf",
+			function()
+				require("zk.commands").get("ZkNotes")({ sort = { "modified" }, match = { vim.fn.input("Search: ") } })
+			end,
+			desc = "Search for notes",
+		},
+		{
+			"<leader>zb",
+			function()
+				require("zk.commands").get("ZkBacklinks")()
+			end,
+			desc = "List note backlinks",
+		},
+		{
+			"<leader>zl",
+			function()
+				require("zk.commands").get("ZkLinks")()
+			end,
+			desc = "List note links",
+		},
 	},
 	init = function()
 		local ok, telescope = pcall(require, "telescope")
