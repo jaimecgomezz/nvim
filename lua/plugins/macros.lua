d9a850731d0fda52fce99ad52239a82f12d5cf30
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-22 23:42:48 -0600

    feat: Add macrothis plugin

---
+++ lua/plugins/macros.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"desdic/macrothis.nvim",
+		lazy = false,
+		opts = {
+			run_register = "q",
+			editor = { -- Edit window
+				width = 80,
+				height = 1,
+				style = "minimal",
+				border = "rounded",
+			},
+			default_register = "q",
+		},
+        -- stylua: ignore
+        keys = {
+            { "<leader>qd", function() require('macrothis').delete() end, desc = "delete" },
+            { "<leader>qe", function() require('macrothis').edit() end, desc = "edit" },
+            { "<leader>ql", function() require('macrothis').load() end, desc = "load" },
+            { "<leader>qr", function() require('macrothis').rename() end, desc = "rename" },
+            { "<leader>qQ", function() require('macrothis').quickfix() end, desc = "run macro on all files in quickfix" },
+            { "<leader>qq", function() require('macrothis').run() end, desc = "run macro" },
+            { "<leader>qs", function() require('macrothis').save() end, desc = "save" },
+            { "<leader>qx", function() require('macrothis').register() end, desc = "edit register" },
+            { "<leader>qy", function() require('macrothis').copy_macro_printable() end, desc = "Copy macro as printable" },
+
+            -- Telescope integration
+            { "<leader>fq", "<CMD> Telescope macrothis <CR>", desc = "list macros" },
+        },
+		init = function()
+			local ok, telescope = pcall(require, "telescope")
+			if ok then
+				require("telescope").load_extension("macrothis")
+			end
+		end,
+	},
+}
--- lua/set.lua
+++ lua/set.lua
@@ -79,7 +79,7 @@ opt.whichwrap:append("<>[]hl")
 
 -- Diagnostics config
 vim.diagnostic.config({
-	update_in_insert = false,
+	update_in_insert = true,
 	underline = false,
 	severity_sort = true,
 	virtual_text = false,
