bc2714017c0179e233c0d072c54937aa678260df
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-24 22:37:41 -0600

    refactor: Improve term toggler/killer

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,70 +1,77 @@
-local function toggle_terminal()
-	local lastbuff = vim.fn.bufnr("#")
-	local termbuff = vim.g.termbuff
-	local currbuff = vim.api.nvim_get_current_buf()
+local function notify(message)
+	vim.notify(message, vim.log.levels.INFO, { title = "Term" })
+end
 
-	-- Prevent opening terminal within another terminals
-	if vim.bo.buftype == "terminal" and currbuff ~= termbuff then
+local function return_to_buffer(prevbuff, termbuff)
+	-- Enter the buffer we were at prior to entering our terminal
+	if vim.fn.buflisted(prevbuff) == 1 and prevbuff ~= termbuff then
+		vim.cmd("buffer" .. prevbuff)
 		return
 	end
 
-	-- Create terminal if not exists
-	if not termbuff then
-		vim.cmd.terminal()
-		vim.g.prevbuff = currbuff
-		vim.g.termbuff = vim.api.nvim_get_current_buf()
-
-	-- Open last edited buffer if we're in terminal
-	elseif currbuff == termbuff then
-		if vim.fn.buflisted(vim.g.prevbuff) == 1 then
-			vim.cmd("buffer" .. vim.g.prevbuff)
-
-		-- Open the last edited buffer
-		elseif vim.fn.buflisted(lastbuff) == 1 then
-			vim.cmd("buffer" .. lastbuff)
+	-- If the buffer we were at prior to entering our terminal isn't listed
+	-- anymore, attempt to enter any of the lastly edited buffers
+	for _, buffer in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.fn.buflisted(buffer) == 1 and buffer ~= termbuff then
+			vim.cmd("silent buffer" .. buffer)
+			return
 		end
+	end
 
-		vim.g.prevbuff = nil
+	-- Assume there's no other listed buffer and open start page
+	vim.cmd.Startify()
+end
 
-		-- Handle no other buffers listed with start page
-		if vim.api.nvim_get_current_buf() == termbuff then
-			vim.cmd.Startify()
-		end
+local function exit_terminal(prevbuff, curbuff, termbuff)
+	-- Prevent creating/entering out terminal if within another terminal, like a
+	-- REPL, Overseer console, Toggleterm, etc
+	if curbuff ~= termbuff then
+		return
+	end
 
-	-- Open terminal if we're not in it
+	return_to_buffer(prevbuff, termbuff)
+end
+
+local function enter_terminal(currbuff, termbuff)
+	-- Switch to existing terminal buffer
+	if vim.fn.buflisted(termbuff) == 1 then
+		vim.cmd("silent buffer" .. termbuff)
+		vim.cmd.startinsert()
 	else
-		vim.cmd("buffer" .. tostring(termbuff))
-		vim.g.prevbuff = currbuff
+		-- Create a terminal buffer
+		vim.cmd("silent terminal")
+
+		vim.g.termbuff = vim.api.nvim_get_current_buf()
 		vim.cmd.startinsert()
 	end
 end
 
-local function kill_terminal()
-	local lastbuff = vim.fn.bufnr("#")
+local function toggle_terminal()
+	local prevbuff = vim.g.prevbuff
 	local termbuff = vim.g.termbuff
 	local currbuff = vim.api.nvim_get_current_buf()
 
-	if not termbuff then
+	if vim.bo.buftype == "terminal" then
+		exit_terminal(prevbuff, currbuff, termbuff)
+	else
+		vim.g.prevbuff = currbuff
+		enter_terminal(currbuff, termbuff)
+	end
+end
+
+local function kill_terminal()
+	-- Do nothing if there's no terminal event set
+	if not vim.g.termbuff then
 		return
 	end
 
+	-- Attempt to delete terminal buffer
+	pcall(vim.cmd, "silent! bdelete! " .. vim.g.termbuff)
+
+	-- Wipe termbuff variable
 	vim.g.termbuff = nil
-	pcall(vim.cmd, "silent! bdelete! " .. termbuff)
 
-	if currbuff == termbuff then
-		-- Open the buffer prior to switching to terminal
-		if vim.fn.buflisted(vim.g.prevbuff) == 1 then
-			vim.cmd("buffer" .. vim.g.prevbuff)
-
-		-- Open the last edited buffer
-		elseif vim.fn.buflisted(lastbuff) == 1 then
-			vim.cmd("buffer" .. lastbuff)
-
-		-- Open Start page
-		else
-			vim.cmd.Startify()
-		end
-	end
+	return_to_buffer(vim.g.prevbuff, vim.g.termbuff)
 end
 
 return {
