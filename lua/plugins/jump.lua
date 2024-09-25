27db8290b2c4bab6784b66662215a37380cb2ceb
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-25 12:26:39 -0600

    refactor: Go back to harpoon, lol

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -1,4 +1,54 @@
 return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup()
+
+			vim.keymap.set("n", "<C-a>", function()
+				local item = harpoon:list():get_by_value(vim.fn.expand("%"))
+
+				if not item then
+					harpoon:list():add()
+				else
+					harpoon:list():remove()
+				end
+			end)
+
+			vim.keymap.set("n", "<leader>hh", function()
+				local file_paths = {}
+				for _, item in ipairs(harpoon:list().items) do
+					table.insert(file_paths, item.value)
+				end
+
+				local tvalues = require("telescope.config").values
+				require("telescope.pickers")
+					.new({}, {
+						prompt_title = "Harpoon",
+						finder = require("telescope.finders").new_table({
+							results = file_paths,
+						}),
+						previewer = tvalues.file_previewer({}),
+						sorter = tvalues.generic_sorter({}),
+					})
+					:find()
+			end, { desc = "Open harpoon window" })
+
+			vim.keymap.set("n", "<leader>hx", function()
+				harpoon:list():clear()
+			end, { desc = "Open harpoon window" })
+
+			for n = 1, 9 do
+				vim.keymap.set("n", "'" .. n, function()
+					harpoon:list():select(n)
+				end)
+			end
+		end,
+	},
+
 	"folke/flash.nvim",
 	event = "VeryLazy",
 	opts = {
