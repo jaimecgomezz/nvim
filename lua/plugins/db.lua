e94d7229f2b85e17ade1496a31d37553053fb2e8
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-25 17:23:04 -0600

    feat: Improve mappings

--- lua/plugins/db.lua
+++ lua/plugins/db.lua
@@ -9,12 +9,20 @@ return {
 		"kristijanhusak/vim-dadbod-ui",
 		-- stylua: ignore
 		keys = {
-		    { "<leader>bb", "<CMD> DBUIToggle <CR>", desc = "Toggle DB UI" },
+		    { "<localleader>dd", "<CMD> DBUIToggle <CR>", desc = "Toggle DB UI" },
+		    { "<localleader>df", "<CMD> DBUIFindBuffer <CR>", desc = "Find query in drawer" },
+		    { "<localleader>dl", "<CMD> DBUILastQueryInfo <CR>", desc = "Find query in drawer" },
         },
-		config = function()
+		-- stylua: ignore
+		init = function ()
+			vim.g.db_ui_use_nvim_notify = 1
+			vim.g.db_ui_show_database_icon = 1
+			vim.g.db_ui_execute_on_save = 0
 			vim.g.db_ui_use_nerd_fonts = 1
 			vim.g.db_ui_show_database_icon = 1
+            vim.g.db_ui_tmp_query_location = "~/tmp/dbui"
 			vim.g.db_ui_hide_schemas = { "pg_catalog", "pg_toast_temp.*" }
-		end,
+		end
+,
 	},
 }
