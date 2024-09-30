b4d535b44c439bfea69bf6584ae54e086d610c34
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-30 10:21:07 -0600

    feat: Update here.term

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -8,5 +8,10 @@ return {
 		opts = {
 			startup_command = "Startify",
 		},
+		-- stylua: ignore
+		keys = {
+			{ "<C-;>", function() require("here-term").toggle_terminal() end, desc = "Toggle terminal", mode = { "n", "t" }, },
+			{ "<C-S-;>", function() require("here-term").kill_terminal() end, desc = "Kill terminal", mode = { "n", "t" }, },
+		},
 	},
 }
