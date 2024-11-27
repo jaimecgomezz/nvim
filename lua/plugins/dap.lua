3bf8a95520bbec020e6ac294f443b468eaec818b
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-11-26 19:13:57 -0600

    feat: Improve dap config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -24,26 +24,127 @@ return {
         },
       },
     },
-    -- stylua: ignore
-      keys = {
-        { "<leader>dB", function() require('persistent-breakpoints.api').set_conditional_breakpoint() end, desc = "Breakpoint Condition" },
-        { "<leader>db", function() require('persistent-breakpoints.api').toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-        { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
-        { "<leader>dg", function() require("dap").run_to_cursor() end, desc = "Go to Line (No Execute)" },
-        { "<leader>dl", function() require("dap").step_into() end, desc = "Step Into" },
-        { "<leader>dL", function() require("dap").step_over() end, desc = "Step Over" },
-        { "<leader>dh", function() require("dap").step_back() end, desc = "Step back" },
-        { "<leader>dH", function() require("dap").step_out() end, desc = "Step Out" },
-        { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-        { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-        { "<leader>dR", function() require("dap").restart() end, desc = "Restart session" },
-        { "<leader>dq", function() require("dap").terminate() end, desc = "Terminate session" },
-        { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-        { "<leader>du", function() require("dap").reverse_continue() end, desc = "Reverse continue" },
-        { "<leader>ds", function() require("dap").pause() end, desc = "Pause" },
-        { "<leader>dw", function () require('persistent-breakpoints.api').set_log_point() end, desc = "Set log point" },
-        { "<leader>dx", function() require('persistent-breakpoints.api').clear_all_breakpoints() end, desc = "Clear breakpoints" },
+    keys = {
+      {
+        "<leader>dB",
+        function()
+          require("persistent-breakpoints.api").set_conditional_breakpoint()
+        end,
+        desc = "Breakpoint Condition",
       },
+      {
+        "<leader>db",
+        function()
+          require("persistent-breakpoints.api").toggle_breakpoint()
+        end,
+        desc = "Toggle Breakpoint",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<leader>dg",
+        function()
+          require("dap").run_to_cursor()
+        end,
+        desc = "Go to Line (No Execute)",
+      },
+      {
+        "<leader>dl",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<leader>dL",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<leader>dh",
+        function()
+          require("dap").step_back()
+        end,
+        desc = "Step back",
+      },
+      {
+        "<leader>dH",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      {
+        "<leader>dj",
+        function()
+          require("dap").down()
+        end,
+        desc = "Down",
+      },
+      {
+        "<leader>dk",
+        function()
+          require("dap").up()
+        end,
+        desc = "Up",
+      },
+      {
+        "<leader>dR",
+        function()
+          require("dap").restart()
+        end,
+        desc = "Restart session",
+      },
+      {
+        "<leader>dq",
+        function()
+          require("dap").terminate()
+        end,
+        desc = "Terminate session",
+      },
+      {
+        "<leader>dr",
+        function()
+          require("dap").repl.toggle()
+        end,
+        desc = "Toggle REPL",
+      },
+      {
+        "<leader>du",
+        function()
+          require("dap").reverse_continue()
+        end,
+        desc = "Reverse continue",
+      },
+      {
+        "<leader>ds",
+        function()
+          require("dap").pause()
+        end,
+        desc = "Pause",
+      },
+      {
+        "<leader>dw",
+        function()
+          require("persistent-breakpoints.api").set_log_point()
+        end,
+        desc = "Set log point",
+      },
+      {
+        "<leader>dx",
+        function()
+          require("persistent-breakpoints.api").clear_all_breakpoints()
+        end,
+        desc = "Clear breakpoints",
+      },
+    },
   },
   {
     "rcarriga/nvim-dap-ui",
@@ -74,16 +175,33 @@ return {
       },
       layouts = {
         {
-          elements = { "scopes", "stacks", "breakpoints" },
+          elements = { "scopes" },
           size = 60,
           position = "left",
         },
+        {
+          elements = { "repl" },
+          size = 10,
+          position = "bottom",
+        },
       },
     },
-    -- stylua: ignore
     keys = {
-      { "<leader>dd", function() require("dapui").toggle() end, desc = "Dap UI" },
-      { "<leader>de", function() require("dapui").eval(vim.fn.getline("."), { context = "repl" }) end, desc = "Eval", mode = {"n", "v"} },
+      {
+        "<leader>dd",
+        function()
+          require("dapui").toggle()
+        end,
+        desc = "Dap UI",
+      },
+      {
+        "<leader>de",
+        function()
+          require("dapui").eval(vim.fn.getline("."), { context = "repl" })
+        end,
+        desc = "Eval",
+        mode = { "n", "v" },
+      },
     },
   },
 }
