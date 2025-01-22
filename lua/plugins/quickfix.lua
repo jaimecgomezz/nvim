8a3c6d75130ffd3a52297a498719db9fecc1cc22
jaimecgomezz <jaimecgomezwork@gmail.com>
2025-01-22 16:37:12 -0600

    feat: Remove `stevearc/quicker.nvim` dependency. Improve `qf` UI.

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,41 +1,83 @@
+local function custom_qfx(_)
+  local fn = vim.fn
+
+  function _G.qftf(info)
+    local items
+    local ret = {}
+    -- The name of item in list is based on the directory of quickfix window.
+    -- Change the directory for quickfix window make the name of item shorter.
+    -- It's a good opportunity to change current directory in quickfixtextfunc :)
+    vim.cmd(("noa lcd %s"):format(fn.fnameescape(vim.fn.getcwd())))
+
+    if info.quickfix == 1 then
+      items = fn.getqflist({ id = info.id, items = 0 }).items
+    else
+      items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+    end
+    local limit = 80
+    local fnameFmt1, fnameFmt2 = "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
+    local validFmt = "%s │%5d:%-3d│%s %s"
+    for i = info.start_idx, info.end_idx do
+      local e = items[i]
+      local fname = ""
+      local str
+      if e.valid == 1 then
+        if e.bufnr > 0 then
+          fname = fn.bufname(e.bufnr)
+          if fname == "" then
+            fname = "[No Name]"
+          else
+            fname = fname:gsub("^" .. vim.env.HOME, "~")
+          end
+          -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
+          if #fname <= limit then
+            fname = fnameFmt1:format(fname)
+          else
+            fname = fnameFmt2:format(fname:sub(1 - limit))
+          end
+        end
+        local lnum = e.lnum > 99999 and -1 or e.lnum
+        local col = e.col > 999 and -1 or e.col
+        local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
+        str = validFmt:format(fname, lnum, col, qtype, e.text)
+      else
+        str = e.text
+      end
+      table.insert(ret, str)
+    end
+    return ret
+  end
+
+  vim.o.qftf = "{info -> v:lua._G.qftf(info)}"
+end
+
 return {
   {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
     opts = {
+      filter = {
+        fzf = {
+          extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
+        },
+      },
       preview = {
         auto_preview = false,
         show_title = false,
         winblend = 0,
       },
     },
-  },
-  {
-    "stevearc/quicker.nvim",
-    ft = "qf",
-    opts = {
-      type_icons = {
-        E = "E ",
-        W = "W ",
-        I = "I ",
-        N = "N ",
-        H = "H ",
-      },
-    },
+    init = custom_qfx,
     keys = {
       {
         "<localleader>q",
-        function()
-          require("quicker").toggle({ focus = true, min_height = 10 })
-        end,
+        "<CMD> copen <CR>",
         desc = "Toggle quickfix",
       },
       {
         "<localleader>l",
-        function()
-          require("quicker").toggle({ loclist = true, focus = true, min_height = 10 })
-        end,
-        desc = "Toggle loclist",
+        "<CMD> lopen <CR>",
+        desc = "Toggle quickfix",
       },
     },
   },
