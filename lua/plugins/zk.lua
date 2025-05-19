f26b652c93fc5eb970e4b453496d0e3ef2f67215
jaimecgomezz <jaimecgomezwork@gmail.com>
2025-05-19 11:40:38 -0600

    feat: Improve zk configuration

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,5 +1,7 @@
-local function pick_and_run(cb)
-  local dirs = vim.split(vim.fn.glob("~/zk/*/"), '\n', { trimempty = true })
+local function select_notebook(cb)
+  cb = cb or function() end
+  local dirs = vim.split(vim.fn.glob("~/zk/*/"), "\n", { trimempty = true })
+
   vim.ui.select(dirs, {
     format_item = function(item)
       local name = item
@@ -9,13 +11,24 @@ local function pick_and_run(cb)
       end
 
       return name
-    end
+    end,
   }, function(choice)
-    if choice ~= nil and choice ~= '' then
-      vim.fn.setenv('ZK_NOTEBOOK_DIR', choice)
+    if choice ~= nil and choice ~= "" then
+      vim.fn.setenv("ZK_NOTEBOOK_DIR", choice)
+      vim.fn.chdir(choice)
       cb()
     end
   end)
+end
+
+local function pick_and_run(cb)
+  local notebook = vim.env.ZK_NOTEBOOK_DIR
+
+  if notebook == nil or notebook == "" then
+    select_notebook(cb)
+  else
+    cb()
+  end
 end
 
 return {
@@ -28,22 +41,24 @@ return {
   keys = {
     {
       "<leader>zz",
-      function()
-        require("zk").cd()
-      end,
+      select_notebook,
       desc = "Move to notes directory",
     },
     {
       "<leader>zi",
       function()
-        require("zk").index()
+        pick_and_run(function()
+          require("zk").index()
+        end)
       end,
       desc = "Index notes",
     },
     {
       "<leader>zo",
       function()
-        require("zk.commands").get("ZkNotes")({ sort = { "modified" } })
+        pick_and_run(function()
+          require("zk.commands").get("ZkNotes")({ sort = { "modified" } })
+        end)
       end,
       desc = "Open note",
     },
@@ -63,39 +78,49 @@ return {
     {
       "<leader>zN",
       function()
-        local title = vim.fn.input("Title: ")
+        pick_and_run(function()
+          local title = vim.fn.input("Title: ")
 
-        if title and title ~= "" then
-          require("zk.commands").get("ZkNew")({ dir = "ideas", title = title })
-        end
+          if title and title ~= "" then
+            require("zk.commands").get("ZkNew")({ dir = "ideas", title = title })
+          end
+        end)
       end,
       desc = "New idea",
     },
     {
       "<leader>zt",
       function()
-        require("zk.commands").get("ZkTags")()
+        pick_and_run(function()
+          require("zk.commands").get("ZkTags")()
+        end)
       end,
       desc = "Search notes by tags",
     },
     {
       "<leader>zf",
       function()
-        require("zk.commands").get("ZkNotes")({ sort = { "modified" } })
+        pick_and_run(function()
+          require("zk.commands").get("ZkNotes")({ sort = { "modified" } })
+        end)
       end,
       desc = "Search notes by name",
     },
     {
       "<leader>zb",
       function()
-        require("zk.commands").get("ZkBacklinks")()
+        pick_and_run(function()
+          require("zk.commands").get("ZkBacklinks")()
+        end)
       end,
       desc = "List note backlinks",
     },
     {
       "<leader>zl",
       function()
-        require("zk.commands").get("ZkLinks")()
+        pick_and_run(function()
+          require("zk.commands").get("ZkLinks")()
+        end)
       end,
       desc = "List note links",
     },
